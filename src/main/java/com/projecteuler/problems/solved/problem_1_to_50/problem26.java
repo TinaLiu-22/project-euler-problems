c1bda6c74da5d0af2fa96d9fd0ef604df391e03b
Tina <xialiu79@yahoo.ca>
2023-12-23 00:02:54 -0500

    Problem 26 solved

---
+++ src/main/java/com/projecteuler/problems/solved/problem_1_to_50/problem26.java
@@ -0,0 +1,32 @@
+package com.projecteuler.problems.solved.problem_1_to_50;
+
+public class problem26 {
+
+    public static void main(String[] args) {
+
+        int upperBound = 1000;
+        int bestDigitCount = 0;
+        int bestDenominator = 0;
+
+        for (int d = 2; d < upperBound; d++) {
+            if (d % 2 == 0 || d % 5 == 0) {
+                continue;
+            }
+
+            int a = 1;
+            int digitCount = 0;
+            do {
+                a = a * 10 % d;
+                digitCount++;
+            } while (a != 1);
+
+            if (bestDigitCount < digitCount) {
+                bestDigitCount = digitCount;
+                bestDenominator = d;
+            }
+        }
+
+        System.out.println(bestDigitCount);
+        System.out.println(bestDenominator);
+    }
+}
